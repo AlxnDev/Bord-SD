f1adc1f0515f14817052057d7d5804455afbef88
AlxnDev <rockmrcontact@gmail.com>
2026-03-22 13:12:54 -0600

    Refactor to define BASE_DIR and run app conditionally
    Define BASE_DIR and conditionally run the app.

--- bord_pro.py
+++ bord_pro.py
@@ -1,8 +1,12 @@
 import os
 import sys
 
+# 1. Primero definimos la ruta base para que esté disponible siempre
+BASE_DIR = os.path.dirname(sys.executable if getattr(sys, 'frozen', False) else __file__)
+
+# 2. Luego importamos lo demás
 from core.app import run
 
-run()
-
-BASE_DIR = os.path.dirname(sys.executable if getattr(sys, 'frozen', False) else __file__)
+# 3. Y SOLO ejecutamos la app si abrimos el archivo directamente
+if __name__ == "__main__":
+    run()
